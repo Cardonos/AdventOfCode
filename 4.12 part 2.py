38b77dcb1f0028aa6a83e50d0ff5ad93c5de73fc
Cardonos <Lasse@Lasse-Stausberg.de>
2022-12-04 12:26:19 +0100

    day 4, shortened solutions

--- 4.12 part 2.py
+++ 4.12 part 2.py
@@ -3,8 +3,8 @@ data = pd.read_csv('Inputs/Day 4.txt', names=['a', 'b', 'c', 'd'], sep=',|-', en
 i = 0; count = 0
 while i < len(data):
     if (data.at[i, 'c'] - data.at[i, 'a']) >= 0 and (data.at[i, 'b'] >= data.at[i, 'c']):
-        count+=1
+        count += 1
     elif (data.at[i, 'a'] - data.at[i, 'c']) >= 0 and (data.at[i, 'd'] >= data.at[i, 'a']):
-        count+=1
-    i+=1
+        count += 1
+    i += 1
 print(count)
