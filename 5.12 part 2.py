75268485d5679f62bf73d81b7d59ad3f0b3f848f
Cardonos <Lasse@Lasse-Stausberg.de>
2022-12-05 15:22:20 +0100

    Day 5, commented

--- 5.12 part 2.py
+++ 5.12 part 2.py
@@ -4,6 +4,7 @@ import numpy as np
 data = pd.read_csv('Inputs/Day 5.txt', header=None, names=['a'], nrows=9)
 commands = pd.read_csv('Inputs/Day 5.txt', header=None, names=['a', 'count', 'b', 'start', 'c', 'end'], sep=' ',
                        skiprows=9)
+# Importing the two halves of the Data: the Box Stack and the list of Instructions
 print(data)
 i = 0
 j = 0
@@ -11,61 +12,63 @@ stack = []
 crates = []
 while i < len(data):
     while j < len(data.at[1, 'a']):
-        stack = np.append(stack, (data.at[i, 'a'][j:j + 3]))
-        j += 4
+        stack = np.append(stack, (data.at[i, 'a'][j:j + 3]))  # building the stack, three characters at a time
+        j += 4  # skipping to the start of the next cell
     if len(crates) == 0:
-        crates = stack
+        crates = stack  # when the output is empty the first value is the input
     else:
-        crates = np.vstack((crates, stack))
+        crates = np.vstack((crates, stack))  # appending the new values in case the output isnt empty
     j = 0
     i += 1
     stack = []
-crates[0, len(crates)-1] = '   '
+crates[0, len(crates) - 1] = '   '  # manually adding a cell because for whatever reason the text input file
+# automatically deletes it
 print(crates)
+
 
 def moveCrate(state, start, end, number):
     r = 0
     moving = []
     space = True
-    while r < number:
+    while r < number:  # iterating over the number of cells to be moved
         k = 0
         if not state[r, end - 1] == '   ':
-            space = False
-        while k < len(state):
-            if not state[k, start-1] == '   ':
+            space = False  # checks whether the stack to be inserted is higher than the current maximum height
+        while k < len(state):  # iterates over the starting stack
+            if not state[k, start - 1] == '   ':  # looking for the first non-empty cell
                 if len(moving) == 0:
-                    moving = [state[k, start-1]]
+                    moving = [state[k, start - 1]]  # creating the moving vector if it does not exist yet
                 else:
-                    moving = np.append(moving, state[k, start-1])
-                state[k, start-1] = '   '
+                    moving = np.append(moving, state[k, start - 1])  # appending to the moving vector
+                state[k, start - 1] = '   '  # replacing the copied cells with empty cells
                 k = len(state)
             else:
                 k += 1
 
         r += 1
     r = 0
-    while r < number:
+    while r < number:  # iterating over the number of cells to be moved
         l = 0
         if space:
             while l < len(state):
                 if not state[l, end - 1] == '   ':
                     state[l - 1, end - 1] = moving[len(moving) - 1 - r]
-                    l = len(state)
+                    l = len(state)  # If there is enough space for the entire movement stack it just gets placed down
                 else:
                     l += 1
-        else:
+        else:  # if there is not enough space for the entire movement stack
             while l < len(state):
-                if not state[0, end - 1] == '   ':
+                if not state[0, end - 1] == '   ':  # if the top cell is filled it creates a new line
                     newline = np.zeros(len(state[0]), dtype=state.dtype)
                     o = 0
                     while o < len(newline):
                         newline[o] = '   '
                         o += 1
                     newline[end - 1] = moving[len(moving) - 1 - r]
-                    state = np.vstack((newline, state))
+                    state = np.vstack((newline, state))  # the to be moved cell gets stacked together with the new row
                     l = len(state)
-                elif not state[l, end - 1] == '   ':
-                    state[l - 1, end - 1] = moving[len(moving) - 1 - r]
+                elif not state[l, end - 1] == '   ':  # checks for the first non-empty cell
+                    state[l - 1, end - 1] = moving[len(moving) - 1 - r]  # places the object in the first empty cell
                     l = len(state)
                 else:
                     l += 1
@@ -76,7 +79,7 @@ def moveCrate(state, start, end, number):
 m = 0
 while m < len(commands):
     crates = moveCrate(crates, commands.at[m, 'start'], commands.at[m, 'end'], commands.at[m, 'count'])
-    m += 1
+    m += 1  # iterates over the list of commands to create the final stack
 print(crates)
 
 output = []
@@ -86,10 +89,9 @@ while p < len(crates[0]):
     while q < len(crates):
         if not crates[q, p] == '   ':
             output.append(crates[q, p])
-            q = len(crates)
+            q = len(crates)  # iterates over the stack to create the final output
         else:
             q += 1
     p += 1
 
 print(output)
-
--- 5.12.py
+++ 5.12.py
@@ -4,6 +4,7 @@ import numpy as np
 data = pd.read_csv('Inputs/Day 5.txt', header=None, names=['a'], nrows=9)
 commands = pd.read_csv('Inputs/Day 5.txt', header=None, names=['a', 'count', 'b', 'start', 'c', 'end'], sep=' ',
                        skiprows=9)
+# Importing the two halves of the Data: the Box Stack and the list of Instructions
 print(data)
 i = 0
 j = 0
@@ -11,51 +12,55 @@ stack = []
 crates = []
 while i < len(data):
     while j < len(data.at[1, 'a']):
-        stack = np.append(stack, (data.at[i, 'a'][j:j + 3]))
-        j += 4
+        stack = np.append(stack, (data.at[i, 'a'][j:j + 3]))  # building the stack, three characters at a time
+        j += 4  # skipping to the start of the next cell
     if len(crates) == 0:
-        crates = stack
+        crates = stack  # when the output is empty the first value is the input
     else:
-        crates = np.vstack((crates, stack))
+        crates = np.vstack((crates, stack))  # appending the new values in case the output isnt empty
     j = 0
     i += 1
     stack = []
-crates[0, len(crates)-1] = '   '
+crates[0, len(crates) - 1] = '   '  # manually adding a cell because for whatever reason the text input file
+# automatically deletes it
 print(crates)
 
-def moveCrate(state, start, end):
+
+def moveCrate(state, start, end):  # routine for moving a crate from a start position to an end position, returns
+    # the new stack
     k = 0
     l = 0
-    while k < len(state):
-        if not state[k, start-1] == '   ':
-            moving = state[k, start-1]
-            state[k, start-1] = '   '
-            k = len(state)
+    while k < len(state):  # iterates over the starting column
+        if not state[k, start - 1] == '   ':  # Checks whether the currently looked at cell is filled
+            moving = state[k, start - 1]
+            state[k, start - 1] = '   '
+            k = len(state)  # copies the object to be moved and replaces it with an empty cell
         else:
             k += 1
-    while l < len(state):
-        if not state[0, end-1] == '   ':
+    while l < len(state):  # iterates over the target column
+        if not state[0, end - 1] == '   ':  # checks whether the top spot of the column is filled
             newline = np.zeros(len(state[0]), dtype=state.dtype)
             o = 0
             while o < len(newline):
                 newline[o] = '   '
                 o += 1
             newline[end - 1] = moving
-            state = np.vstack((newline, state))
+            state = np.vstack((newline, state))  # creates a new row with the target cell filled and stacks it
             l = len(state)
-        elif not state[l, end-1] == '   ':
-            state[l - 1, end-1] = moving
-            l = len(state)
+        elif not state[l, end - 1] == '   ':  # checks whether the target cell is filled
+            state[l - 1, end - 1] = moving
+            l = len(state)  # places the copied object in the first free cell
         else:
             l += 1
-    return state
+    return state  # returns the new stack
+
 
 m = 0
 while m < len(commands):
     n = 0
     while n < commands.at[m, 'count']:
         crates = moveCrate(crates, commands.at[m, 'start'], commands.at[m, 'end'])
-        n += 1
+        n += 1  # Iterates over the commands to create the final state of the stack
     m += 1
 print(crates)
 
@@ -66,10 +71,9 @@ while p < len(crates[0]):
     while q < len(crates):
         if not crates[q, p] == '   ':
             output.append(crates[q, p])
-            q = len(crates)
+            q = len(crates)  # iterates over the stack to create the final output
         else:
             q += 1
     p += 1
 
 print(output)
-
