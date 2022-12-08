e028edac192ccf85afe0358d1a0c9bf6559971c4
lasse <lasse@lasse-stausberg.de>
2022-12-08 12:10:40 +0100

    day 8 part 1

--- 8.12.py
+++ 8.12.py
@@ -3,43 +3,38 @@ import numpy as np
 
 data = pd.read_csv('Inputs/Day 8.txt', header=None)
 print(data)
-
-indexing = []
-i = 0
-while i < len(str(data.iat[0, 0])):
-    indexing.extend([str(i)])
-    i += 1
-treeGrid = []
+# Parsing the Data into a grid
+treeGrid = []   # creating the empty grid
 j = 0
 while j < len(data):
     newLine = []
     k = 0
-    while k < len(str(data.iat[j, 0])):
+    while k < len(str(data.iat[j, 0])): # populating the new line with Trees
         newLine.append(int(str(data.iat[j, 0])[k]))
         k += 1
-    if len(treeGrid) > 0:
+    if len(treeGrid) > 0:   # adding the new line to the grid
         treeGrid = np.vstack((treeGrid, newLine))
     else:
         treeGrid = newLine
     j += 1
 
-visibleGrid = np.zeros(treeGrid.shape)
+visibleGrid = np.zeros(treeGrid.shape)  # defining the grid to track visibility, 0 means covered, 1 means seen
 
 
 def checkvisibility(direction, trees, visibility):
-    if direction == 'left' or direction == 'right':
+    if direction == 'left' or direction == 'right': # Checking fo which direction is being looked at
         lookingdir = 1
     elif direction == 'up' or direction == 'down':
         lookingdir = 0
     m = 0
-    while m < trees.shape[abs(lookingdir-1)]:
-        currentHighestTree = -1
+    while m < trees.shape[abs(lookingdir-1)]:   # iterating over the Axis not being looked at
+        currentHighestTree = -1 # variable to track the currently highest tree looked at
         n = 0
-        if direction == 'left':
+        if direction == 'left': # Routines for checking each direction
             while n < trees.shape[lookingdir]:
-                if currentHighestTree < trees[m, n]:
-                    currentHighestTree = trees[m, n]
-                    visibility[m, n] = 1
+                if currentHighestTree < trees[m, n]:    # if the tree is taller than the current highest
+                    currentHighestTree = trees[m, n]    # new highest Tree
+                    visibility[m, n] = 1    #tree is visible
                 n += 1
         elif direction == 'right':
             while n < trees.shape[lookingdir]:
@@ -61,11 +56,11 @@ def checkvisibility(direction, trees, visibility):
                 n += 1
         m += 1
 
-print(treeGrid[0, 1])
-
 checkvisibility('down', treeGrid, visibleGrid)
 checkvisibility('up', treeGrid, visibleGrid)
 checkvisibility('left', treeGrid, visibleGrid)
-checkvisibility('right', treeGrid, visibleGrid)
+checkvisibility('right', treeGrid, visibleGrid) # checking every direction
 print(visibleGrid)
-print(np.count_nonzero(visibleGrid == 1))
+print(np.count_nonzero(visibleGrid == 1))   # counting the number of visible trees
+
+# Part 2
